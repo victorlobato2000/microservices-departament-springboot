27d2e0c49cef7f10e7d303ad655500e8040c68ad
victorlobato2000 <victorsilva123190@gmail.com>
2021-10-26 16:04:00 -0300

    First Commit

---
+++ src/main/java/com/victorlobatomicrosservico/departamento/DepartamentoServicoApplication.java
@@ -0,0 +1,13 @@
+package com.victorlobatomicrosservico.departamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartamentoServicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartamentoServicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victorlobatomicrosservico/departamento/DepartamentoServicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorlobatomicrosservico.departamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartamentoServicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
